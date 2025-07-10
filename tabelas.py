39fea07d1bcbe68dce908bc3c6c5e51333036e33
Rafael Perazzo <rafaelperazzo@gmail.com>
2025-07-10 13:28:03 -0300

    feat: Exportação para CSV concluida

--- tabelas.py
+++ tabelas.py
@@ -21,10 +21,32 @@ def posicao_segunda_cidade(linha):
     return -1
 
 def posicao_segundo_estado(linha):
-    for i, item in enumerate(linha):
-        if '(' in item and i > posicao_primeiro_estado(linha):
+    for i in range(len(linha)-1, 0, -1):
+        if '(' in linha[i]:
             return i
     return -1
+
+def remover_hifem(linhas):
+    linhas_ajustadas = []
+    for linha in linhas:
+        for i, item in enumerate(linha):
+            if '-' in item:
+                linha.pop(i)
+            #if ')' in item:
+            #    linha[i] = item.replace(')', '')
+            #    linha[i] = linha[i].replace('(', '')
+        linhas_ajustadas.append(linha)
+    return linhas_ajustadas
+
+def remover_parenteses(linhas):
+    linhas_ajustadas = []
+    for linha in linhas:
+        for i, item in enumerate(linha):
+            if '(' in item:
+                linha[i] = item.replace('(', '')
+                linha[i] = linha[i].replace(')', '')
+        linhas_ajustadas.append(linha)
+    return linhas_ajustadas
 
 def ajustar_linhas_primeira_cidade(linhas):
     linhas_ajustadas = []
@@ -38,6 +60,8 @@ def ajustar_linhas_primeira_cidade(linhas):
                 linha[1] = cidade.strip()
                 linha = [linha[0], linha[1]] + linha[estado:]
                 linhas_ajustadas.append(linha)
+            else:
+                linhas_ajustadas.append(linha)
         else:
             linhas_ajustadas.append(linha)
     return linhas_ajustadas
@@ -45,15 +69,16 @@ def ajustar_linhas_primeira_cidade(linhas):
 def ajustar_linhas_segunda_cidade(linhas):
     linhas_ajustadas = []
     for i, linha in enumerate(linhas):
-        if len(linha)>10: #Linha com nome de uma das cidades composto
+        if len(linha)>9: #Linha com nome da segunda cidade composto
             estado = posicao_primeiro_estado(linha)
-            if estado > 2: #Primeira cidade com nome composto - Tratar
-                cidade = ''
-                for i in range(1, estado):
-                    cidade = cidade + ' ' + linha[i]
-                linha[1] = cidade.strip()
-                linha = [linha[0], linha[1]] + linha[estado:]
-                linhas_ajustadas.append(linha)
+            estado2 = posicao_segundo_estado(linha)
+            
+            cidade = ''
+            for i in range(estado+1, estado2):
+                cidade = cidade + ' ' + linha[i]
+            linha[3] = cidade.strip()
+            linha = [linha[0], linha[1],linha[2],linha[3]] + linha[estado2:]
+            linhas_ajustadas.append(linha)
         else:
             linhas_ajustadas.append(linha)
     return linhas_ajustadas
@@ -77,6 +102,11 @@ with pdfplumber.open('planilha.pdf') as pdf:
             linhas.remove(linha)
     linhas = remover_pontos(linhas)
     linhas = ajustar_linhas_primeira_cidade(linhas)
-    #linhas = ajustar_linhas_segunda_cidade(linhas)
+    linhas = remover_hifem(linhas)
+    linhas = ajustar_linhas_segunda_cidade(linhas)
+    linhas = remover_parenteses(linhas)
+    print(len(linhas))
     for linha in linhas:
         print(len(linha), linha)
+    df = pd.DataFrame(linhas, columns=['UF', 'Cidade1', 'Cidade2', 'Estado1', 'Estado2','Ida','Volta','Total','pass.km'])
+    df.to_csv('tabela.csv', index=False, encoding='utf-8')
