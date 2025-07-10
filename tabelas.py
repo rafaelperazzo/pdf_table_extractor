accbaddd733ef40b99a2bd321839cc7ab354a61a
Rafael Perazzo <rafaelperazzo@gmail.com>
2025-07-10 11:10:58 -0300

    primeiro commit

---
+++ tabelas.py
@@ -0,0 +1,62 @@
+import pdfplumber 
+import pandas as pd
+
+def remover_pontos(linhas):
+    for linha in linhas:
+        for i, item in enumerate(linha):
+            if '.' in item:
+                linha[i] = item.replace('.', '')
+    return linhas
+
+def posicao_primeiro_estado(linha):
+    for i, item in enumerate(linha):
+        if '(' in item:
+            return i
+    return -1
+
+def posicao_segunda_cidade(linha):
+    for i, item in enumerate(linha):
+        if '-' in item:
+            return i+1
+    return -1
+
+def posicao_segundo_estado(linha):
+    for i, item in enumerate(linha):
+        if '(' in item and i > posicao_primeiro_estado(linha):
+            return i
+    return -1
+
+def ajustar_linhas(linhas):
+    for i, linha in enumerate(linhas):
+        if len(linha)>10: #Linha com nome de uma das cidades composto
+            estado = posicao_primeiro_estado(linha)
+            if estado > 2: #Primeira cidade com nome composto - Tratar
+                cidade = ''
+                for i in range(1, estado):
+                    cidade = cidade + ' ' + linha[i]
+                for i in range(2,estado):
+                    linha.remove(linha[i])
+                linha[1] = cidade.strip()
+                
+    return linhas
+        
+with pdfplumber.open('planilha.pdf') as pdf:
+    for i, page in enumerate(pdf.pages):
+        texto = page.extract_text()
+        if i == 0:
+            linhas = texto.split('\n')
+            linhas = linhas[7:]
+        else:
+            linhas += texto.split('\n')
+    for i,linha in enumerate(linhas):
+        if 'Fonte' in linha:
+            linhas = linhas[:i-1]
+            break
+    linhas = [linha.split() for linha in linhas]
+    for linha in linhas:
+        if len(linha) == 5:
+            linhas.remove(linha)
+    linhas = remover_pontos(linhas)
+    linhas = ajustar_linhas(linhas)
+    for linha in linhas:
+        print(len(linha), linha)
